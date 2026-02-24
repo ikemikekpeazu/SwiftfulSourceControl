908ffa476e4f21cc643bbe63a4f9c4620281cc91
Ikem <iikekpeazu@gmail.com>
2026-02-24 12:39:57 -0500

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ikem Ikekpeazu on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
