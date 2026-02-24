3e1259a9e541b57179fb94763214f1fbc6805aae
Ikem <iikekpeazu@gmail.com>
2026-02-24 13:32:15 -0500

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ikem Ikekpeazu on 2/24/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
