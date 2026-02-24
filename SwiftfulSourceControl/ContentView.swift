5e780f921e96cab822167a16bffd5a57a82eee36
Ikem <iikekpeazu@gmail.com>
2026-02-24 13:24:14 -0500

    Another commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,9 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello!")
             
-            Button("Click me!"){
-                
-            }
+            
         }
         .padding()
     }
