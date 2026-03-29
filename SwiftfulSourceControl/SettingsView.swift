56a584051c1c36cabbcbb40de1a5e63025deb15b
Ikem <iikekpeazu@gmail.com>
2026-03-29 11:22:50 -0400

    add settings view

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Ikem Ikekpeazu on 3/29/26.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
